9d721642cdfca4bc24eca130d8aba54b6c283f46
Amanda Savluchinske <amandasavluchinske@gmail.com>
2020-09-30 21:14:21 -0300

    Adds solution for problem_4, still missing the explanation

--- problem_4/problem_4.py
+++ problem_4/problem_4.py
@@ -27,15 +27,40 @@ sub_child = Group("subchild")
 sub_child_user = "sub_child_user"
 sub_child.add_user(sub_child_user)
 
+parent_user = "parent_user"
+
 child.add_group(sub_child)
 parent.add_group(child)
+parent.add_user(parent_user)
+
+
+def get_all_users_in_groups(user, group):
+    users = group.get_users()
+    groups = group.get_groups()
+
+    for group in groups:
+        users.extend(get_all_users_in_groups(user, group))
+
+    return users
+
 
 def is_user_in_group(user, group):
-    """
-    Return True if user is in the group, False otherwise.
+    if not user or not group:
+        return False
+    return user in get_all_users_in_groups(user, group)
 
-    Args:
-      user(str): user name/id
-      group(class:Group): group to check user membership against
-    """
-    return None
+
+print('Should return True:', is_user_in_group(parent_user, parent))
+# returns True
+
+print('Should return True:', is_user_in_group(sub_child_user, parent))
+# returns True
+
+print('Should return False:', is_user_in_group(parent_user, child))
+# returns False
+
+print('Should return False:', is_user_in_group(None, child))
+# returns False
+
+print('Should return False:', is_user_in_group(parent_user, None))
+# returns False
